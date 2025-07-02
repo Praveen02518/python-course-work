c068f02cf4d9b7319f8ae642d6fd344ba794efd6
Praveen02518 <kunchalapraveenbabu@gmail.com>
2025-07-02 12:51:10 +0530

    Python programs

---
+++ Classwork/4.operators.py
@@ -0,0 +1,72 @@
+#1.Arithmetic Operators
+a=20
+b=10
+print("Addition(+):",a+b) #Addition(+): 30
+print("Subtraction(-):",a-b) #Subtraction(-): 10
+print("Multiplication(*):",a*b) #Multiplication: 200
+print("Division(/):",a/b) #Division (/):2.0
+print("Floor division(//):",a//b) #Floor division(//):2
+print("Modulus(%):",a%b) #Modulus(%): 0
+print("Exponentiation(**):",a**b) #Exponentiation(**):10240000000000
+#Exmples
+a=20
+b=10
+print(a+b) #30 addition
+print(a-b) #10 subtraction
+print(a*b) #200 multi[lication
+print(a/b) #2.0 division
+print(a//b) #2 floor division
+print(a%b) #0 modulus
+print(b%a) #10 modulus
+print(a**b) #exponentiation
+
+#2.Comparison operators
+a=20
+b=10
+print("Equal to(==):",a==b) #Equal to(==)): False
+print("Not Equal to(!=):",a!=b) #Not Equal to(!=)): True 
+print("Greater than(>):",a>b) #Greater than(>)): True
+print("Less than(<):",a<b) #Less than(<)): False
+print("Greater than or Equal to(>=):",a>=b) #Greater than or Equal to(>=)): True
+print("Less than or Equal to(<=):",a<=b) #Less than or Equal to(<=)): False
+#Exmples
+a=20
+b=10
+print(a==b) #Equal to(==): False
+print(a!=b) #Not Equal to(!=): True
+print(a>b) #Greater than(>): True
+print(a<b) #Less than(<): False
+print(a>=b) #Greater than or Equal to(>=): True
+print(a<=b) #Less than or Equal to(<=): False
+
+#3.Assignment operators
+a=20
+b=10
+a=b
+print("Assign(=):",a) 
+a+=b
+print("Add & Assign(+=):",a)
+print("Subtract & Assign(-=):",a) 
+print("Multiply & Assign(*=):",a)
+print("Divide & Assign(/=):",a) 
+print("Floor divide & Assign(//=):",a) 
+print("Modulus & Assign(%=):",a) 
+print("EXponentiate & Assign(**=):",a)
+
+#4.Logical Operators
+a=10
+b=20
+#AND gate
+print(a>5 and b<30) #true (both conditions are true)
+#OR Gate
+print(a>15 or b<30) #True (atleast one condition is true)
+#NOT gate
+print(not(a>5)) #false (reverse the true condition)
+
+
+
+
+
+
+
+
