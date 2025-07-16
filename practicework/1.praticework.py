eb77b963e8006543521c001deada41be18eeca46
Praveen02518 <kunchalapraveenbabu@gmail.com>
2025-07-16 20:04:07 +0530

    python programs

--- practicework/1.praticework.py
+++ practicework/1.praticework.py
@@ -23,6 +23,28 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
 else:
     print(f"{year} is NOT a Leap Year.")
     
+#string and type conversuion
+date_input = input("Enter a date (dd/mm/yyyy): ")
+day, month, year = date_input.split('/')
+another_format = f"{year}/{month}/{day}"
+print("Date in yyyy/mm/dd format:", another_format)
+
+#even or odd winner
+day = int(input("Enter the day as an integer: "))
+if day % 2 == 0:
+    print("Even Winner")
+else:
+    print("Odd Winner")
+#
+# List of names with duplicates
+names = ["meena", "arun", "arun", "ravi", "jhon", "jhon"]
+unique_names = list(dict.fromkeys(names))
+print("Unique names in order:", unique_names)
+
+
+
+    
+
        
  
  
