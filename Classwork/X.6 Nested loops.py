af9d88a667dd6afb8fa99486d2b4c9a74f1f00af
Praveen02518 <kunchalapraveenbabu@gmail.com>
2025-07-17 12:33:38 +0530

    python programs

---
+++ Classwork/X.6 Nested loops.py
@@ -0,0 +1,25 @@
+#
+n = 5 # Change this for different sizes
+for i in range(n):
+    for j in range(n):
+        print('1', end=' ')
+    print()
+    
+# right angled triangle
+for i in range(n):
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()  
+#   
+#inverted rightangled triangle
+for i in range(6, 0, -1):
+    for j in range(i):
+        print('*', end=' ')
+    print()         
+# pyramid
+n = 5
+for i in range(n):
+    print(' ' * (n - i - 1) + '* ' * (i + 1)) 
+#
+ 
+          
